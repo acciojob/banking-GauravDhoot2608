f7e997b5603ddf3c4b13d161d8578d36866faed0
Gaurav Dhoot <gauravdhoot17@gmail.com>
2024-01-04 15:50:38 +0530

    Project Committed 5

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -6,7 +6,7 @@ public class SavingsAccount extends BankAccount{
 
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-    	super(name, balance, 0.0);
+    	super(name, balance, 0);
     	this.rate = rate;
     	this.maxWithdrawalLimit = maxWithdrawalLimit;
     }
@@ -24,7 +24,7 @@ public class SavingsAccount extends BankAccount{
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
     	
-    	double interest = (getBalance() * rate * years) / 100;
+    	double interest = (getBalance() * (rate/100) * years);
     	
     	return getBalance() + interest;
     }
@@ -32,11 +32,13 @@ public class SavingsAccount extends BankAccount{
     public double getCompoundInterest(int times, int years){
         // Return the final amount considering that bank gives compound interest on current amount given times per year
     	
-    	double ratio = 1 + (rate/(times*100));
-    	
-    	double mul = Math.pow(ratio, years*times);
-    	
-    	return getBalance()*mul;
+    	double P = getBalance();
+        double r = rate/100;
+        int n = times;
+        int t = years;
+        double A = P * (Math.pow((1 + (r/n)), (n*t)));
+        
+        return A;
     }
 
 	public double getRate() {
